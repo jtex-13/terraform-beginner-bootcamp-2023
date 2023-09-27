49884b9949a89fa3c9fe6d7918c495660c953e6e
jtex 13 <jtex-13@hotmail.com>
2023-09-27 22:16:53 +0000

    #21-Change Structure of Root Module and Add tagging to Bucket

--- main.tf
+++ main.tf
@@ -1,48 +1,3 @@
-# Backend
-#erraform {
-# backend "remote" {
-#   hostname     = "app.terraform.io"
-#   organization = "jtex-cloud-learning"
-#
-#   workspaces {
-#     prefix = "terra-house-01"
-#   }
-#}
-#
-
-# Terraform Cloud
-terraform {
-  cloud {
-    organization = "jtex-cloud-learning"
-
-    workspaces {
-      name = "terra-house-01"
-    }
-  }
-}
-
-# Random Provider
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }    
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-provider "aws" {
-  # Configuration options
-}
-
 #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower            = true
@@ -56,13 +11,9 @@ resource "aws_s3_bucket" "example" {
   #Bucket Name Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
  bucket = random_string.bucket_name.result
- }
 
+  tags = {
+    UserUuid = var.user_uuid
+  }
 
-output "random_bucket_name" {
-    value = random_string.bucket_name.id
-}
-
-output "random_bucket_result" {
-    value = random_string.bucket_name.result
 }
