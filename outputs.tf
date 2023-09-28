401138228d8196db5beb3f987505c5b11e222fb7
jtex-13 <90216134+jtex-13@users.noreply.github.com>
2023-09-27 22:12:35 -0400

    #23-Bucket should no longer use random provider (#30)

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "random_bucket_name" {
-    value = random_string.bucket_name.result
+output "bucket_name" {
+  value = aws_s3_bucket.website_bucket.bucket
 }
-
-#output "random_bucket_result" {
-#    value = random_string.bucket_name.result
-#}
--- providers.tf
+++ providers.tf
@@ -24,10 +24,6 @@
 # Random Provider
 terraform {
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source = "hashicorp/aws"
       version = "5.17.0"
