38a1febecd3efee2c1455f9e930e9fb70dc528d5
jtex-13 <90216134+jtex-13@users.noreply.github.com>
2023-10-07 12:21:17 -0400

    #52-To test our terra home on terratowns test town. (#53)

--- main.tf
+++ main.tf
@@ -7,6 +7,32 @@ terraform {
   }
 }
 
+provider "terratowns" {
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
+}
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
+
+resource "terratowns_home" "home" {
+  name = "Family of Coders in 2023!"
+  description = <<DESCRIPTION
+Family Coders is a retreat conducted that allows their family members to learn how to code.
+Also an oppurtunity for the coder parents to spend time and share their hidden talents to their kids.
+DESCRIPTION
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
+  content_version = 1
+}
+
 #module "terrahouse_aws" {
 #  source              = "./modules/terrahouse_aws"
 #  user_uuid           = var.user_uuid
@@ -16,21 +42,3 @@ terraform {
 #  content_version     = var.content_version
 #  assets_path         = var.assets_path
 #}
-
-provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
-}
-
-resource "terratowns_home" "home" {
-  name = "Family of Coders in 2023!"
-  description = <<DESCRIPTION
-Family Coders is a retreat conducted that allows their family members to learn how to code.
-Also an oppurtunity for the coder parents to spend time and share their hidden talents to their kids.
-DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "coder-retreat"
-  content_version = 1
-}
