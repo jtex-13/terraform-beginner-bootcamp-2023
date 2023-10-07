8b3cab489fcf86809bafaa158895ffed589649dc
jtex-13 <90216134+jtex-13@users.noreply.github.com>
2023-10-06 22:11:12 -0400

    47 terratowns provider (#51)
    * #47-wip setting up the skeleton for creating a custom terraform provider
    
    * #47-wip additional commits
    
    * #47-wip custom provdier updates
    
    * #47-Implement terraform provider block in terraform.
    
    * #47-Setup skeleton for resource.
    
    * #47-Finish the implementation for terraform provider.

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,6 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:z6xYKzCUuy/CmsflbLtgTMHQRz1zgWzN0oOvwIzRqxI=",
+    "h1:oh6M40GiADYzubo8oYJqO39C/ZQ4kdDTl31crtJgmss=",
   ]
 }
--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "1.0.0"
     }
   }
-}  
+}
 
 #module "terrahouse_aws" {
 #  source              = "./modules/terrahouse_aws"
@@ -21,4 +21,16 @@ provider "terratowns" {
   endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+}
+
+resource "terratowns_home" "home" {
+  name = "Family of Coders in 2023!"
+  description = <<DESCRIPTION
+Family Coders is a retreat conducted that allows their family members to learn how to code.
+Also an oppurtunity for the coder parents to spend time and share their hidden talents to their kids.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "coder-retreat"
+  content_version = 1
 }
